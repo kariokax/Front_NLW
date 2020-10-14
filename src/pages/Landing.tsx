cc13be22fdb5fb0e4cc496dd7dc05c0c729b98ff
kariokax <felipe.c.machado123@gmail.com>
2020-10-14 17:18:11 -0300

    Criação da página mapa de orfanatos

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import{FiArrowRight} from 'react-icons/fi';
+import{Link} from 'react-router-dom' //fazer transição de uma tela pra outra sem recarregar pag totalmente, reapoveita os componentes 
+
+import'../styles/pages/landing.css';
+
+import logoImg from '../images/logo.svg';
+function Landing(){
+  return(
+    <div id="page-landing">
+     <div className="content-wrapper"> 
+          <img src={logoImg} alt="Happy"/>
+
+          <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças</p>
+          </main>
+
+          <div className="location">
+            <strong>Goiânia</strong>
+            <span>Goiás</span>
+          </div>
+
+          <Link to="/app" className="enter-app">
+           <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+          </Link>
+     </div>
+    </div>
+  );
+}
+
+export default Landing;
